f9b85f6a5dc2ba8a62a634199721956cb293e73e
sekar N <sekarnagarajan1234@gmail.com>
2023-05-27 19:16:10 +0530

    portfolio

--- LinkedIn Clone/script.js
+++ LinkedIn Clone/script.js
@@ -1,5 +1,8 @@
-let profileMenu = document.getElementById("profileMenu");
+// let profileMenu = document.getElementById("profileMenu");
 
-function toggleMenu() {
-  profileMenu.classList.toggle("open-menu");
-}
+// function toggleMenu() {
+//   profileMenu.classList.toggle("open-menu");
+// }
+
+let exp = 3e4 ** 22;
+console.log(exp);
